d8b3fbbb7f5d32e8b4993cad0bcc9817a907cf67
lauraoliveri <lauraoliveri2003@gmail.com>
2024-10-22 18:14:10 +0200

    Create model, migration, controller and seeder

---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Comic::truncate();
+
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $comic = new Comic();
+            
+            $comic->title = $comic['title'];
+            $comic->description = $comic['description'];
+            $comic->thumb = $comic['thumb'];
+            $comic->price = $comic['price'];
+            $comic->series = $comic['series'];
+            $comic->type = $comic['type'];
+            $comic->sale_date = $comic['sale_date'];
+
+            $comic->save();
+        }
+    }
+}
