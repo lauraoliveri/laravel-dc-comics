b70000d5e2e05ad4de802deebc0bc020c753652c
lauraoliveri <lauraoliveri2003@gmail.com>
2024-10-28 17:30:03 +0100

    Edit Model

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'type',
+        'series',
+        'price',
+        'description',
+        'sale_date',
+        'thumb'
+    ];
 }
